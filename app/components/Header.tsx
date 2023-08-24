79afd33fd3457f199d3accaa43438278c37acc42
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-08-24 19:20:45 +0500

    done to responsive

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,17 +5,29 @@ import {
   AiOutlineUser,
   AiOutlineFundProjectionScreen,
   AiOutlineContacts,
+  AiOutlineMenu,
 } from "react-icons/ai";
 import { BiSortDown } from "react-icons/bi";
 
 export default function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
   
   return (
     <div className="fixed w-full z-40">
       <div className="container mx-auto py-4">
+        <div className="md:hidden pr-3 flex justify-end">
+          <button onClick={toggleMobileMenu}>
+            <AiOutlineMenu className="w-8 h-8"/>
+            
+          </button>
+        </div>
         <div className="flex justify-between items-center">
           <div className="text-white text-lg font-semibold">
-            <p>Shahzad</p>
+            <p className="-my-7 lg:my-0 pl-3 lg:pl-0">Shahzad</p>
           </div>
           <div className="hidden md:flex gap-6 font-semibold">
             <NavItem icon={<AiOutlineHome />} label="Home" />
@@ -27,6 +39,24 @@ export default function Header() {
          
         </div>
       </div>
+      {mobileMenuOpen && (
+        <div className="fixed top-12 left-end w-full bg-slate-800 ">
+          <div className="mt-2 py-2 px-4 rounded flex flex-col gap-2 text-xl">
+          <NavItem icon={<AiOutlineHome />} label="Home" />
+          <NavItem icon={<AiOutlineUser />} label="About" />
+          <NavItem
+            icon={<AiOutlineFundProjectionScreen />}
+            label="Project"
+          />
+          <NavItem
+            icon={<BiSortDown />}
+            label="Resume"
+            link="/public/ShahzadAli.pdf"
+          />
+          <NavItem icon={<AiOutlineContacts />} label="Contact" />
+        </div>
+        </div>
+      )}
     </div>
   );
 }
