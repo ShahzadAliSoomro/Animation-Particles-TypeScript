ec44d3e963bd0135832da9b286bd454ba5aeb74c
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-08-27 01:01:13 +0500

    done about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,37 @@
-import React from 'react'
-import About from '../components/About'
-import Footer from '../components/Footer'
+import React from "react";
+import About from "../components/About";
+import Footer from "../components/Footer";
 
 export default function Page() {
   return (
-    <div>
-        <div>
-            <About />
+    <div className="w-full pt-20">
+      <div className="w-full">
+        <div className="mx-auto container flex justify-center items-center text-center">
+          <div className="p-8 space-y-8 text-white">
+            <h2 className="lg:text-5xl text-3xl font-bold">
+              Welcome to Our Portfolio
+            </h2>
+            <div className="flex justify-center items-center">
+              <p className="w-full lg:w-5/12">
+                Explore our latest projects and discover the creativity and
+                innovation that defines our work. We take pride in delivering
+                exceptional solutions tailored to your needs.
+              </p>
+            </div>
+            <div className="">
+              <button className="bg-[#fff] text-black z-50 p-3 font-semibold text-lg rounded shadow-md hover:-translate-y-3 transition-all duration-500">
+                Get Inspired
+              </button>
+            </div>
+          </div>
         </div>
-        {/* <div className='justify-evenly'>
+      </div>
+      <div>
+        <About />
+      </div>
+      {/* <div className='justify-evenly'>
             <Footer />
         </div> */}
-        
     </div>
-  )
+  );
 }
