59d0d2a35a10459191fc3b1d4e182c774e513374
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-08-30 16:02:53 +0500

    changes done

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -19,21 +19,21 @@ export default function Footer() {
           <div className="text-lg">
             <p>Copyright © 2023 SZ</p>
           </div>
-          <div className="flex gap-5 cursor-pointer">
+          <div className="flex gap-5 cursor-pointer text-gray-400">
           <Link href="https://github.com/ShahzadAliSoomro">
-              <TiSocialGithubCircular className="w-8 h-8 hover:bg-green-500 rounded-full" />
+              <TiSocialGithubCircular className="w-9 h-9 hover:bg-green-500 hover:text-white rounded-full" />
             </Link>
             <Link href="https://www.linkedin.com/in/shahzad-ali-51748122b/">
-              <TiSocialLinkedinCircular className="w-8 h-8 hover:bg-green-500 rounded-full" />
+              <TiSocialLinkedinCircular className="w-9 h-9 hover:bg-green-500 hover:text-white rounded-full" />
             </Link>
             <Link href="https://www.facebook.com/profile.php?id=100067543970671&mibextid=ZbWKwL">
-              <TiSocialFacebookCircular className="w-8 h-8 hover:bg-green-500 rounded-full" />
+              <TiSocialFacebookCircular className="w-9 h-9 hover:bg-green-500 hover:text-white rounded-full" />
             </Link>
             <Link href="https://twitter.com/Shahzad67656720?t=VTYdy-uo4cClxdvEI_4DiA&s=08">
-              <TiSocialTwitterCircular className="w-8 h-8 hover:bg-green-500 rounded-full" />
+              <TiSocialTwitterCircular className="w-9 h-9 hover:bg-green-500 hover:text-white rounded-full" />
             </Link>
             <Link href="https://instagram.com/shahzadhussain1731043?igshid=MzNlNGNkZWQ4Mg==">
-                <TiSocialInstagramCircular className="w-8 h-8 hover:bg-green-500 rounded-full" />
+                <TiSocialInstagramCircular className="w-9 h-9 hover:bg-green-500 hover:text-white rounded-full" />
             </Link>
           </div>
         </div>
