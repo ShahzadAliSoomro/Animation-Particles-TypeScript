37e1af0e390b0bec635dee18d3941dd740b01a56
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-08-31 16:30:34 +0500

    changes done

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -80,69 +80,59 @@ export default function Hero() {
   const { heading, subheading, emphasis } = texts[currentIndex];
 
   return (
-    <div className="w-full relative h-[60vh]">
-    <div className="max-w-[1440px] mx-auto container lg:p-10 p-4 ">
-      <div className="flex justify-between pt-20 flex-wrap gap-6 lg:flex-nowrap lg:gap-0">
-        <div className="flex flex-col gap-9">
-          <h2 className="text-4xl">Hi There!</h2>
-          <h1 className="text-6xl">I'M SHAHZAD ALI</h1>
-          <div>
-            <h1 className="lg:text-2xl text-lg font-bold flex gap-2 items-center">
-              {heading}{" "}
-              <span className="lg:text-5xl text-xl">
-                {emphasis.slice(0, currentCharIndex + 1)}
-              </span>
-            </h1>
-            <p className="lg:text-xl text-lg">{subheading}</p>
-          </div>
-
-          <div className="absolute top-[480px] left-[580px] cursor-pointer hidden md:block">
-            <div className="flex gap-3 items-center border p-2 rounded-full w-[500px] ">
-              {iconData.map((icon, index) => (
-                <Link key={index} href={icon.link} className="relative group">
-                  <div
-                    className={`relative transition-all rounded-full text-gray-400 flex items-center ${
-                      hoveredIcon === index
-                        ? "border-2 border-green-500 text-white"
-                        : ""
-                    }`}
-                    onMouseEnter={() => setHoveredIcon(index)}
-                    onMouseLeave={() => setHoveredIcon(null)}
-                  >
-                    {icon.icon}
-                    {hoveredIcon === index && (
-                      <div className="text-white py-1 px-3 text-lg font-semibold">
-                        {icon.text}
-                      </div>
-                    )}
-                  </div>
-                </Link>
-              ))}
+    <div className="w-full relative h-[61vh]">
+      <div className="max-w-[1440px] mx-auto container lg:p-10 p-4">
+        <div className="flex justify-between pt-20 flex-wrap gap-6 lg:flex-nowrap lg:gap-0">
+          <div className="flex flex-col gap-9">
+            <h2 className="text-4xl">Hi There!</h2>
+            <h1 className="text-6xl">I'M SHAHZAD ALI</h1>
+            <div>
+              <h1 className="lg:text-2xl text-lg font-bold flex gap-2 items-center">
+                {heading}{" "}
+                <span className="lg:text-5xl text-xl">
+                  {emphasis.slice(0, currentCharIndex + 1)}
+                </span>
+              </h1>
+              <p className="lg:text-xl text-lg">{subheading}</p>
+            </div>
+            <div className="absolute top-[480px] left-[580px] cursor-pointer hidden md:block">
+              <div className="flex gap-3 items-center border p-2 rounded-full w-[500px]">
+                {iconData.map((icon, index) => (
+                  <Link key={index} href={icon.link} className="relative group">
+                    <div
+                      className={`relative transition-all rounded-full text-gray-400 flex items-center ${
+                        hoveredIcon === index
+                          ? "border-2 border-green-500 text-white"
+                          : ""
+                      }`}
+                      onMouseEnter={() => setHoveredIcon(index)}
+                      onMouseLeave={() => setHoveredIcon(null)}
+                    >
+                      {icon.icon}
+                      {hoveredIcon === index && (
+                        <div className="text-white py-1 px-3 text-lg font-semibold">
+                          {icon.text}
+                        </div>
+                      )}
+                    </div>
+                  </Link>
+                ))}
+              </div>
             </div>
           </div>
         </div>
-        {/* <div className="absolute top-12 right-0">
-          <Image
-            src="/hero3.avif"
-            alt="hero"
-            width={500}
-            height={500}
-            className="w-[600px] h-[550px]"
-          />
-        </div> */}
+        <Design />
       </div>
-      <Design />
-    </div>
-    <div className="w-[900px] h-[575px] absolute top-14 right-0 hidden md:block bg-[#011A26] ">
-          <Image
-            src="/vec-d.png"
-            alt="hero"
-            width={1000}
-            height={1000}
-            className="w-[600px] h-[605px] object-cover absolute top-0 right-[150px]"
-          />
+      <div className="w-[900px] h-[575px] absolute top-14 right-0 hidden md:block bg-[#011A26] ">
+        <Image
+          src="/vec-d.png"
+          alt="hero"
+          width={1000}
+          height={1000}
+          className="w-[600px] h-[605px] object-cover absolute top-0 right-[150px]"
+        />
 
-<div className="flex justify-between px-32 pt-10">
+        <div className="flex justify-between px-32 pt-10">
           <Image
             src="/react.png"
             alt="react"
@@ -174,9 +164,7 @@ export default function Hero() {
             className="w-[100px] h-[100px] object-cover animate-spin"
           />
         </div>
-
-          
-        </div>
+      </div>
     </div>
   );
 }
