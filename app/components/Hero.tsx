75fcc35399d573a107a88b816b38800656e99eeb
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-09-01 20:13:43 +0500

    changes

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -96,13 +96,13 @@ export default function Hero() {
   return (
     <div className="w-full">
       <div className="max-w-[1440px] mx-auto container lg:p-10 p-4">
-        <div className="flex justify-between pt-24">
+        <div className="flex justify-between pt-24 flex-wrap lg:flex-nowrap gap-5">
           <div className="w-full lg:w-6/12">
             <div className="flex flex-col gap-12 lg:pt-10 pt-0">
               <h2 className="text-4xl">Hi There!</h2>
               <h1 className="text-6xl">I'M SHAHZAD ALI</h1>
               <div>
-                <h1 className="lg:text-2xl text-lg font-bold flex gap-2 items-center">
+                <h1 className="lg:text-2xl text-sm font-bold flex gap-2 items-center">
                   {heading}{" "}
                   <span className="lg:text-5xl text-xl">
                     {emphasis.slice(0, currentCharIndex + 1)}
@@ -110,7 +110,7 @@ export default function Hero() {
                 </h1>
                 <p className="lg:text-xl text-lg">{subheading}</p>
               </div>
-              <div className="md:flex md:gap-3 items-center border p-2 rounded-full w-full md:w-[500px]">
+              <div className="md:flex md:gap-3 items-center border flex p-2 rounded-full w-full md:w-[500px]">
                 {iconData.map((icon, index) => (
                   <Link key={index} href={icon.link} className="relative group">
                     <div
@@ -124,7 +124,7 @@ export default function Hero() {
                     >
                       {icon.icon}
                       {hoveredIcon === index && (
-                        <div className="text-white py-1 px-3 text-lg font-semibold">
+                        <div className="text-white py-1 px-3 lg:text-lg text-sm font-semibold">
                           {icon.text}
                         </div>
                       )}
@@ -136,120 +136,196 @@ export default function Hero() {
           </div>
 
           <div
-            className="w-full lg:w-6/12 hidden md:block 
+            className="w-full lg:w-6/12 
           "
           >
             <div className="flex justify-end">
-              <div className="">
+              <div className="relative">
                 <Image
                   src="/vec-d.png"
                   alt="vector"
                   width={1000}
                   height={1000}
-                  className="w-[580px] h-[580px] object-cover "
+                  className="lg:w-[580px] lg:h-[580px] object-cover"
                 />
               </div>
               {showIcons && (
-                <div className="absolute top-[200px] right-[100px]">
-                  <div className="flex flex-col gap-5">
-                    <div className="flex gap-[300px] px-[60px]">
-                      <div data-aos="fade-up-left" data-aos-duration="2500">
-                        <Image
-                          src="/react.png"
-                          alt="react"
-                          width={100}
-                          height={100}
-                          className="w-[80px] h-[80px] rounded-full animate-fadeInFromCenter animate-spin"
-                        />
-                      </div>
-                      <div
-                        data-aos="fade-up-right"
-                        data-aos-duration="2500"
-                        className="mr-[100px]"
-                      >
-                        <Image
-                          src="/nodejs.png"
-                          alt="node"
-                          width={100}
-                          height={100}
-                          className="w-[73px] h-[73px] rounded animate-fadeInFromCenter animate-spin"
-                        />
-                      </div>
+                <div className="absolute top-[88%] md:top-[15%] lg:top-[200px] left-[10%] md:left-[10%] lg:left-[350px]">
+                <div className="flex flex-col gap-5">
+                  <div className="flex md:flex-row lg:flex-row gap-[70%] md:gap-[20%] lg:gap-[30%] px-[10%] md:px-[10%] lg:px-[15%]">
+                    <div data-aos="fade-up-left" data-aos-duration="2500">
+                      <Image
+                        src="/react.png"
+                        alt="react"
+                        width={100}
+                        height={100}
+                        className="w-[100%] md:w-[15%] lg:w-[15%] h-auto rounded-full animate-fadeInFromCenter animate-spin"
+                      />
                     </div>
-                    <div className="flex gap-[420px]">
-                      <div data-aos="fade-up-left" data-aos-duration="2500">
-                        <Image
-                          src="/javascript.png"
-                          alt="react"
-                          width={100}
-                          height={100}
-                          className="w-[90px] h-[90px] rounded-full animate-fadeInFromCenter animate-spin"
-                        />
-                      </div>
-                      <div
-                        data-aos="fade-up-right"
-                        data-aos-duration="2500"
-                        className=""
-                      >
-                        <Image
-                          src="/tailwind.png"
-                          alt="node"
-                          width={100}
-                          height={100}
-                          className="w-[73px] h-[73px] rounded-full animate-fadeInFromCenter animate-spin"
-                        />
-                      </div>
+                    <div
+                      data-aos="fade-up-right"
+                      data-aos-duration="2500"
+                      className="mr-[0%] ml-2 md:ml-0 lg:ml-0 md:mr-[15%] lg:mr-[20%]"
+                    >
+                      <Image
+                        src="/nodejs.png"
+                        alt="node"
+                        width={100}
+                        height={100}
+                        className="w-[100%] md:w-[12%] lg:w-[12%] h-auto rounded animate-fadeInFromCenter animate-spin"
+                      />
                     </div>
-                    <div className="flex gap-[480px]">
-                      <div data-aos="fade-up-left" data-aos-duration="2500">
-                        <Image
-                          src="/html.png"
-                          alt="react"
-                          width={100}
-                          height={100}
-                          className="w-[80px] h-[80px] rounded-full animate-fadeInFromCenter animate-spin"
-                        />
-                      </div>
-                      <div
-                        data-aos="fade-up-right"
-                        data-aos-duration="2500"
-                        className="rounded-e-full "
-                      >
-                        <Image
-                          src="/typescript.png"
-                          alt="node"
-                          width={100}
-                          height={100}
-                          className="w-[73px] h-[73px] animate-fadeInFromCenter animate-spin"
-                        />
-                      </div>
+                  </div>
+                  <div className="flex justify-between md:flex-row lg:flex-row gap-[70%] md:gap-[20%] lg:gap-[30%] px-[0%] md:px-[10%] lg:px-[15%]">
+                    <div data-aos="fade-up-left" data-aos-duration="2500" className="ml-[4%]">
+                      <Image
+                        src="/javascript.png"
+                        alt="javaScript"
+                        width={100}
+                        height={100}
+                        className="w-[100%] md:w-[18%] lg:w-[18%] h-auto rounded-full animate-fadeInFromCenter animate-spin"
+                      />
                     </div>
-                    <div className="flex gap-[550px]">
-                      <div data-aos="fade-up-left" data-aos-duration="2500">
-                        <Image
-                          src="/nextjs.png"
-                          alt="next"
-                          width={160}
-                          height={100}
-                          className="w-[140px] h-[100px] object-cover rounded-full animate-fadeInFromCenter animate-spin"
-                        />
-                      </div>
-                      <div
-                        data-aos="fade-up-right"
-                        data-aos-duration="2500"
-                        className=""
-                      >
-                        <Image
-                          src="/express.png"
-                          alt="express"
-                          width={100}
-                          height={100}
-                          className="w-[83px] h-[83px] object-cover animate-fadeInFromCenter animate-spin"
-                        />
-                      </div>
+                    <div
+                      data-aos="fade-up-right"
+                      data-aos-duration="2500"
+                      className="mr-[0%] ml-[5%] md:ml-0 lg:ml-0 md:mr-[15%] lg:mr-[20%]"
+                    >
+                      <Image
+                        src="/tailwind.png"
+                        alt="tailwind"
+                        width={100}
+                        height={100}
+                        className="w-[90%] md:w-[12%] lg:w-[12%] h-auto rounded-full animate-fadeInFromCenter animate-spin"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex md:flex-row lg:flex-row gap-[40%] md:gap-[20%] lg:gap-[30%] px-[0%] md:px-[10%] lg:px-[15%]">
+                    <div data-aos="fade-up-left" data-aos-duration="2500">
+                      <Image
+                        src="/html.png"
+                        alt="react"
+                        width={100}
+                        height={100}
+                        className="w-[38%] md:w-[15%] lg:w-[15%] h-auto rounded-full animate-fadeInFromCenter animate-spin"
+                      />
+                    </div>
+                    <div
+                      data-aos="fade-up-right"
+                      data-aos-duration="2500"
+                      className=""
+                    >
+                      <Image
+                        src="/typescript.png"
+                        alt="node"
+                        width={100}
+                        height={100}
+                        className="w-[35%] md:w-[12%] lg:w-[12%] h-auto ml-[80%] animate-fadeInFromCenter animate-spin"
+                      />
                     </div>
                   </div>
                 </div>
+              </div>
+                // <div className="absolute lg:top-[20%]  -right-[10%]">
+                //   <div className="flex flex-col gap-5">
+                //     <div className="flex gap-[300px] px-[130px]">
+                //       <div data-aos="fade-up-left" data-aos-duration="2500">
+                //         <Image
+                //           src="/react.png"
+                //           alt="react"
+                //           width={100}
+                //           height={100}
+                //           className="w-[20%] md:w-[15%] lg:w-[15%] h-auto rounded-full animate-fadeInFromCenter animate-spin"
+                //         />
+                //       </div>
+                //       <div
+                //         data-aos="fade-up-right"
+                //         data-aos-duration="2500"
+                //         className="mr-[100px]"
+                //       >
+                //         <Image
+                //           src="/nodejs.png"
+                //           alt="node"
+                //           width={100}
+                //           height={100}
+                //           className="w-[15%] md:w-[12%] lg:w-[12%] h-auto rounded animate-fadeInFromCenter animate-spin"
+                //         />
+                //       </div>
+                //     </div>
+                //     <div className="flex gap-[390px] px-[80px]">
+                //       <div data-aos="fade-up-left" data-aos-duration="2500">
+                //         <Image
+                //           src="/javascript.png"
+                //           alt="react"
+                //           width={100}
+                //           height={100}
+                //           className="w-[90px] h-[90px] rounded-full animate-fadeInFromCenter animate-spin"
+                //         />
+                //       </div>
+                //       <div
+                //         data-aos="fade-up-right"
+                //         data-aos-duration="2500"
+                //         className=""
+                //       >
+                //         <Image
+                //           src="/tailwind.png"
+                //           alt="node"
+                //           width={100}
+                //           height={100}
+                //           className="w-[73px] h-[73px] rounded-full animate-fadeInFromCenter animate-spin"
+                //         />
+                //       </div>
+                //     </div>
+                //     <div className="flex gap-[450px] px-[60px]">
+                //       <div data-aos="fade-up-left" data-aos-duration="2500">
+                //         <Image
+                //           src="/html.png"
+                //           alt="react"
+                //           width={100}
+                //           height={100}
+                //           className="w-[80px] h-[80px] rounded-full animate-fadeInFromCenter animate-spin"
+                //         />
+                //       </div>
+                //       <div
+                //         data-aos="fade-up-right"
+                //         data-aos-duration="2500"
+                //         className="rounded-e-full "
+                //       >
+                //         <Image
+                //           src="/typescript.png"
+                //           alt="node"
+                //           width={100}
+                //           height={100}
+                //           className="w-[73px] h-[73px] animate-fadeInFromCenter animate-spin"
+                //         />
+                //       </div>
+                //     </div>
+                //     {/* <div className="flex gap-[550px]">
+                //       <div data-aos="fade-up-left" data-aos-duration="2500">
+                //         <Image
+                //           src="/nextjs.png"
+                //           alt="next"
+                //           width={160}
+                //           height={100}
+                //           className="w-[140px] h-[100px] object-cover rounded-full animate-fadeInFromCenter animate-spin"
+                //         />
+                //       </div>
+                //       <div
+                //         data-aos="fade-up-right"
+                //         data-aos-duration="2500"
+                //         className=""
+                //       >
+                //         <Image
+                //           src="/express.png"
+                //           alt="express"
+                //           width={100}
+                //           height={100}
+                //           className="w-[83px] h-[83px] object-cover animate-fadeInFromCenter animate-spin"
+                //         />
+                //       </div>
+                //     </div> */}
+                //   </div>
+                // </div>
               )}
             </div>
           </div>
