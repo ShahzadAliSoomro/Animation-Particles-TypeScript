5036df27ad358820f2c5fcdf66cb478c384cffa0
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-09-01 23:05:23 +0500

    changes

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -150,7 +150,7 @@ export default function Hero() {
                 />
               </div>
               {showIcons && (
-                <div className="absolute top-[69%] md:top-[15%] lg:top-[200px] left-[10%] md:left-[10%] lg:left-[63%]">
+                <div className="absolute top-[100%] md:top-[15%] lg:top-[200px] left-[10%] md:left-[10%] lg:left-[63%]">
                 <div className="flex flex-col gap-5">
                   <div className="flex md:flex-row lg:flex-row gap-[70%] md:gap-[20%] lg:gap-[30%] px-[10%] md:px-[10%] lg:px-[15%]">
                     <div data-aos="fade-up-left" data-aos-duration="2500">
@@ -165,7 +165,7 @@ export default function Hero() {
                     <div
                       data-aos="fade-up-right"
                       data-aos-duration="2500"
-                      className="mr-[0%] ml-2 md:ml-0 lg:ml-0 md:mr-[15%] lg:mr-[20%]"
+                      className="mr-[0%] ml-6 md:ml-0 lg:ml-0 md:mr-[15%] lg:mr-[20%]"
                     >
                       <Image
                         src="/nodejs.png"
