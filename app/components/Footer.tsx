d96cc8b213e7a34f241717a6d91d81135a46045b
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-09-04 02:43:19 +0500

    changes

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -20,25 +20,35 @@ export default function Footer() {
             <p>Copyright © 2023 SZ</p>
           </div>
           <div className="flex gap-5 cursor-pointer text-gray-400 ">
-          <Link href="https://github.com/ShahzadAliSoomro">
-              <TiSocialGithubCircular className="w-9 h-9 hover:bg-green-500 hover:text-white rounded-full 
-              hover:-translate-y-2 transition-all duration-500" />
+            <Link href="https://github.com/ShahzadAliSoomro">
+              <TiSocialGithubCircular
+                className="w-9 h-9 hover:bg-green-500 hover:text-white rounded-full 
+              hover:-translate-y-2 transition-all duration-500"
+              />
             </Link>
             <Link href="https://www.linkedin.com/in/shahzad-ali-51748122b/">
-              <TiSocialLinkedinCircular className="w-9 h-9 hover:bg-green-500 hover:text-white rounded-full
-              hover:-translate-y-2 transition-all duration-500" />
+              <TiSocialLinkedinCircular
+                className="w-9 h-9 hover:bg-green-500 hover:text-white rounded-full
+              hover:-translate-y-2 transition-all duration-500"
+              />
             </Link>
             <Link href="https://www.facebook.com/profile.php?id=100067543970671&mibextid=ZbWKwL">
-              <TiSocialFacebookCircular className="w-9 h-9 hover:bg-green-500 hover:text-white rounded-full
-              hover:-translate-y-2 transition-all duration-500" />
+              <TiSocialFacebookCircular
+                className="w-9 h-9 hover:bg-green-500 hover:text-white rounded-full
+              hover:-translate-y-2 transition-all duration-500"
+              />
             </Link>
             <Link href="https://twitter.com/Shahzad67656720?t=VTYdy-uo4cClxdvEI_4DiA&s=08">
-              <TiSocialTwitterCircular className="w-9 h-9 hover:bg-green-500 hover:text-white rounded-full
-              hover:-translate-y-2 transition-all duration-500" />
+              <TiSocialTwitterCircular
+                className="w-9 h-9 hover:bg-green-500 hover:text-white rounded-full
+              hover:-translate-y-2 transition-all duration-500"
+              />
             </Link>
             <Link href="https://instagram.com/shahzadhussain1731043?igshid=MzNlNGNkZWQ4Mg==">
-                <TiSocialInstagramCircular className="w-9 h-9 hover:bg-green-500 hover:text-white rounded-full
-                hover:-translate-y-2 transition-all duration-500" />
+              <TiSocialInstagramCircular
+                className="w-9 h-9 hover:bg-green-500 hover:text-white rounded-full
+                hover:-translate-y-2 transition-all duration-500"
+              />
             </Link>
           </div>
         </div>
