dcfd478d8f7c54e8ef6452aa696578faa4a031a0
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-09-04 04:41:56 +0500

    changes

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -5,11 +5,14 @@ export default function About() {
   return (
     <div className="w-full bg-[#1E293B] ">
       <div className="max-w-[1340px] mx-auto container lg:p-10 p-4">
+        {/* About Me Title */}
         <div className="text-4xl flex flex-col justify-center items-center gap-1">
           <h1 className="">About Me</h1>
           <p className="border-b-2 w-44 border-green-500"></p>
         </div>
+        {/* About Me Content */}
         <div className="w-full flex flex-col lg:flex-row pt-12 lg:space-x-10">
+          {/* Left Section with Image */}
           <div className="w-full lg:w-6/12 mb-10 lg:mb-0" data-aos="fade-right">
             <Image
               src="/prr.avif"
@@ -19,11 +22,14 @@ export default function About() {
               className="mx-auto"
             />
           </div>
+
+          {/* Right Section with Text */}
           <div className="w-full lg:w-6/12 flex flex-col gap-6">
             <h3 className="text-4xl font-semibold">
               Hi There! I'm Shahzad Ali
             </h3>
             <h5 className="text-2xl text-green-500">Web Developer</h5>
+            {/* Description */}
             <p className="text-lg text-gray-400">
               I am a skilled Web Developer with a passion for crafting seamless
               web experiences. Proficient in HTML, CSS, JavaScript, and React, I
@@ -31,6 +37,8 @@ export default function About() {
               to provide innovative solutions that engage users and enhance
               their online interactions.
             </p>
+
+            {/* Personal Information */}
             <div className="flex flex-col gap-4 text-lg text-gray-400">
               <div className="flex gap-9">
                 <p className="font-semibold">Birthday:</p>
@@ -57,6 +65,8 @@ export default function About() {
                 <p>Availble</p>
               </div>
             </div>
+            
+            {/* Download CV Button */}
             <div className="mt-3">
               <button className="bg-green-500 text-lg p-4 py-2 rounded-full hover:-translate-y-3 transition-all duration-500">
                 Download CV
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,50 @@
-import React from 'react'
-import Hero from './components/Hero'
-import About from './components/About'
-import Project from './components/Project'
-import Contact from './components/Contact'
-import Service from './components/Service'
-import Skills from './components/Skills'
-
+import React from "react";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Project from "./components/Project";
+import Contact from "./components/Contact";
+import Service from "./components/Service";
+import Skills from "./components/Skills";
 
 export default function Home() {
   return (
-    
-    <div className='w-full text-white'>
-    <Hero />
-    <About />
-    <Service />
-    <Skills />
-    <Project />
-    <Contact />
-  </div>
-
-  )
+    <div className="w-full text-white">
+      {/* Here's what each part of the code does: 
+      1. The Hero component contains various state variables to manage animations and interactions. It also
+      includes data for text and social media icons. 
+      2. The useEffect hook is used to delay the display of icons (showIcons) after a certain time. 
+      3. The iconData array contains information about social media icons along with their links. 
+      4. Another useEffect hook is used for animating the text in the hero section. 
+      5. The component renders the hero section, including text, social media icons, and an image. 
+      6. The Image component from Next.js is used to display an image. 
+      7. The animated icons are displayed with the help of the data-aos attribute, and the ParticlesAnimation
+      component is also rendered. 
+      To use this Hero component, you can include it
+      in your main application file or in any page where you want to display the
+      hero section. Just make sure to import the necessary dependencies and set
+      up any required styling and animations. */}
+      <Hero />
+      {/* Here's what each part of the code does:
+      1. The "About" component is a section of your webpage that provides information about you, 
+      including your image, job title, description, and personal details.
+      2. It uses the next/image package to display an image efficiently. You have provided the 
+      path to the image (src), its alt attribute, and its dimensions (width and height).
+      3. The component is divided into two sections: the left section containing the image and 
+      the right section containing text.
+      4. The "About Me" title is displayed at the top, followed by an underlined separator.
+      5. In the right section, your name, job title, and a description of yourself are displayed.
+      6. Below the description, there is a section with your personal information, including your 
+      birthday, phone number, email, location, languages, and availability.
+      7. Finally, a "Download CV" button is provided with hover animation effects.
+      To use this "About" component, you can include it in your main application file or in any 
+      page where you want to display information about yourself. Simply import the component and 
+      render it within your desired page or layout. */}
+      <About />
+      
+      <Service />
+      <Skills />
+      <Project />
+      <Contact />
+    </div>
+  );
 }
